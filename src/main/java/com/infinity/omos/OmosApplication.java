660373b6857066b0df5c902b68974537defc8377
sumeen99 <70104259+sumeen99@users.noreply.github.com>
2022-02-09 23:53:34 +0900

    OMOS 프로젝트 시작

---
+++ src/main/java/com/infinity/omos/OmosApplication.java
@@ -0,0 +1,13 @@
+package com.infinity.omos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infinity/omos/OmosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinity.omos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
