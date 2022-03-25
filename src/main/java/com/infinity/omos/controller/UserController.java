1ce43a7f9a4c70078fdddedaf723961c43c3c931
sumeen99 <70104259+sumeen99@users.noreply.github.com>
2022-03-25 21:20:40 +0900

    유저 신고 API 오류 수정

--- src/main/java/com/infinity/omos/controller/UserController.java
+++ src/main/java/com/infinity/omos/controller/UserController.java
@@ -3,6 +3,7 @@ package com.infinity.omos.controller;
 import com.infinity.omos.domain.UserRepository;
 import com.infinity.omos.dto.*;
 import com.infinity.omos.service.AuthService;
+import com.infinity.omos.service.PostsService;
 import com.infinity.omos.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/api/user")
@@ -20,6 +22,7 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
     private final AuthService authService;
+    private final PostsService postsService;
 
 
     @ApiOperation(value = "내 프로필 불러오기 API",notes = "지금은 내 프로필 불러오기로 한건데.. 나중에 다른 사람 프로필도 불러올 게 생긴다면 이거 쓰시면 될것같아요!")
@@ -43,6 +46,8 @@ public class UserController {
     @ApiOperation(value = "유저 신고API",notes = "현재는 한번만 신고당해도 바아로 삭제")
     @PutMapping("/{userId}/report")
     public ResponseEntity<StateDto> reportUser(@PathVariable Long userId){
+        List<MyRecordDto> myRecordDtoList = postsService.selectMyPosts(userId);
+        myRecordDtoList.stream().map(MyRecordDto::getRecordId).collect(Collectors.toList()).forEach(postsService::delete);
         return ResponseEntity.ok(authService.signOut(userId));
     }
 
