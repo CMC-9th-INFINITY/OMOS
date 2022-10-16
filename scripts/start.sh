f4930287dc966f2a976f86385f5dc5c397997de5
sumeen99 <70104259+sumeen99@users.noreply.github.com>
2022-10-17 03:24:19 +0900

    scripts 파일 작성

---
+++ scripts/start.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# start.sh
+# 서버 구동을 위한 스크립트
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+REPOSITORY=/home/ec2-user/app
+PROJECT_NAME=OMOS
+
+echo "> Build 파일 복사"
+echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
+
+cp $REPOSITORY/deloy/*.jar $REPOSITORY/
+
+echo "> 새 어플리케이션 배포"
+JAR_NAME=omos-0.0.1-SNAPSHOT.jar
+#$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+IDLE_PROFILE=$(find_idle_profile)
+
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
+nohup java -jar \
+    -Dspring.config.location=/home/ec2-user/app/application.yml,/home/ec2-user/app/application-$IDLE_PROFILE.yml \
+    #-Dspring.profiles.active=$IDLE_PROFILE \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
