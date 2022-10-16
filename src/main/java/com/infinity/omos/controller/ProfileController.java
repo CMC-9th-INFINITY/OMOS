f5d432a6a8c1fd3bb434a61bef1601c37ba5a67e
sumeen99 <70104259+sumeen99@users.noreply.github.com>
2022-10-17 02:54:30 +0900

    nginx 관련 api 추가

---
+++ src/main/java/com/infinity/omos/controller/ProfileController.java
@@ -0,0 +1,28 @@
+package com.infinity.omos.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/profile")
+public class ProfileController {
+    private final Environment env;
+
+    @GetMapping("")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real1", "real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
+        // real, real1, real2 중 하나라도 있으면 그 값 반환
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+}
