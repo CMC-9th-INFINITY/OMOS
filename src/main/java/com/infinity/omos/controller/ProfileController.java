aea005ed4bdf8721f9ad10d6195971b714adb909
sumeen99 <70104259+sumeen99@users.noreply.github.com>
2022-10-17 16:32:49 +0900

    restcontroller로 변경

--- src/main/java/com/infinity/omos/controller/ProfileController.java
+++ src/main/java/com/infinity/omos/controller/ProfileController.java
@@ -2,18 +2,20 @@ package com.infinity.omos.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@RestController
 @RequiredArgsConstructor
-@RequestMapping("/profile")
 public class ProfileController {
     private final Environment env;
 
-    @GetMapping("")
+    @GetMapping("/profile")
     public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real1", "real2");
