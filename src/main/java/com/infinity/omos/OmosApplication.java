0e2325881887d91e62f9f3654f5f4625ddd6176e
sumeen99 <70104259+sumeen99@users.noreply.github.com>
2023-01-15 12:09:26 +0900

    [01.15] feign 설정

--- src/main/java/com/infinity/omos/OmosApplication.java
+++ src/main/java/com/infinity/omos/OmosApplication.java
@@ -2,10 +2,12 @@ package com.infinity.omos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableFeignClients
 public class OmosApplication {
 
 	public static void main(String[] args) {
